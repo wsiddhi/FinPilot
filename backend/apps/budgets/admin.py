495ec1271f613905b3a163f2dceceeef2feb8928
Siddhi Waghmare <waghmaresiddhi1607@gmail.com>
2026-06-09 19:54:04 +0530

    feat: add budget model

--- backend/apps/budgets/admin.py
+++ backend/apps/budgets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Budget
 
-# Register your models here.
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "category",
+        "amount",
+        "month",
+        "year",
+    )
--- backend/apps/budgets/models.py
+++ backend/apps/budgets/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Budget(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="budgets"
+    )
+
+    category = models.ForeignKey(
+        "categories.Category",
+        on_delete=models.CASCADE,
+        related_name="budgets"
+    )
+
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    month = models.IntegerField()
+
+    year = models.IntegerField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.category.name} - ₹{self.amount}"
